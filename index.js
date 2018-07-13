73ab12fde7da9c25f108624b3e793d7132962025
Rafael Alfaro <rafaela@thehangar.cr>
2018-07-13 14:13:24 -0600

    changes text message

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ export default class Hello360 extends React.Component {
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
           <Text style={styles.greeting}>
-            Welcome to React 360
+            Welcome to React VR Example Project
           </Text>
         </View>
       </View>
