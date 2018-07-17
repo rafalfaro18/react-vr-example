09d8f08e63dc099157530f713d249e1c2be8e23f
Rafael Alfaro <rafaela@thehangar.cr>
2018-07-17 16:45:46 -0600

    grabs heroku's dynamic port variable

--- build-server.js
+++ build-server.js
@@ -1,6 +1,6 @@
 const nodeStatic = require('node-static');
 const file = new nodeStatic.Server('./build');
-const port = 3000;
+const port = process.env.PORT || 3000;
 console.log('\nListening on port: ' + port);
 require('http').createServer(function(request, response) {
   request.addListener('end', function() {
