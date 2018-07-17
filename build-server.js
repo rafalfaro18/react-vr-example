e51774df13fe9e89126c2b92cfcdd33c2d7f3a5f
Rafael Alfaro <rafaela@thehangar.cr>
2018-07-17 16:58:32 -0600

    adds comments

--- build-server.js
+++ build-server.js
@@ -1,5 +1,7 @@
 const nodeStatic = require('node-static');
 const file = new nodeStatic.Server('./build');
+//Heroku uses random port and forwards to port 80 automatically
+//port 3000 is for running locally
 const port = process.env.PORT || 3000;
 console.log('\nListening on port: ' + port);
 require('http').createServer(function(request, response) {
